3c1ef35d87a5e9e3fcb6745c5e470b890bf084b6
kolur_20 <kolur_20>
2024-04-16 22:02:53 +0300

    wip

---
+++ src/net/OzonCard.Common.Infrastructure/Database/Migrations/Operational/20240416184943_AddUserIdFieldMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OzonCard.Common.Infrastructure.Database.Migrations.Operational
+{
+    /// <inheritdoc />
+    public partial class AddUserIdFieldMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "organizations_members",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "organizations_members");
+        }
+    }
+}
