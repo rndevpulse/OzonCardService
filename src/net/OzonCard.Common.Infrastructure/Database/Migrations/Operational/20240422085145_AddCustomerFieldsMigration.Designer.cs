68c36608408d338f50abbda557ffd24ada2861c4
kolur_20 <kolur_20>
2024-04-22 11:55:58 +0300

    wip

--- src/net/OzonCard.Common.Infrastructure/Database/Migrations/Operational/20240422085145_AddCustomerFieldsMigration.Designer.cs
+++ src/net/OzonCard.Common.Infrastructure/Database/Migrations/Operational/20240422085145_AddCustomerFieldsMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OzonCard.Common.Infrastructure.Database;
 
@@ -11,9 +12,11 @@ using OzonCard.Common.Infrastructure.Database;
 namespace OzonCard.Common.Infrastructure.Database.Migrations.Operational
 {
     [DbContext(typeof(InfrastructureContext))]
-    partial class InfrastructureContextModelSnapshot : ModelSnapshot
+    [Migration("20240422085145_AddCustomerFieldsMigration")]
+    partial class AddCustomerFieldsMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
