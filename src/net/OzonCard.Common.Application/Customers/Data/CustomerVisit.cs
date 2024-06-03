2c83b9610eba3af5ccc5d7b943b9b4078138944c
kolur_20 <kolur_20>
2024-06-03 12:57:33 +0300

    add customer in sync

--- src/net/OzonCard.Common.Application/Customers/Data/CustomerVisit.cs
+++ src/net/OzonCard.Common.Application/Customers/Data/CustomerVisit.cs
@@ -1,7 +1,17 @@
 ﻿namespace OzonCard.Common.Application.Customers.Data;
 
-public record CustomerVisit(
-    string? Card,
-    DateTimeOffset LastVisitDate,
-    int DaysGrant
-);
+public class CustomerVisit
+{
+    public string? Card { get; }
+    private List<DateTimeOffset> Visits { get; }
+    public DateTimeOffset LastVisitDate {get;}
+    public int DaysGrant {get;}
+    public CustomerVisit(string? card, IEnumerable<DateTimeOffset> visits)
+    {
+        Card = card;
+        Visits = visits.ToList();
+        LastVisitDate = Visits.Max(r => r);
+        DaysGrant = Visits.GroupBy(t => t).Count();
+
+    }
+}
