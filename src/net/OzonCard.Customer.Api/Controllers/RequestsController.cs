26d2affb1cea83baf3f1ec1a0000317102df01f8
kolur_20 <kolur_20>
2024-10-23 13:30:13 +0300

    deferred controller

---
+++ src/net/OzonCard.Customer.Api/Controllers/RequestsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OzonCard.Customer.Api.Models.BackgroundTask;
+using OzonCard.DeferredRequest.Handler;
+using OzonCard.DeferredRequest.Manager;
+using OzonCard.DeferredRequest.Properties.Extensions;
+
+namespace OzonCard.Customer.Api.Controllers;
+
+public class RequestsController(
+    IDeferredRequestsManager manager
+) : ApiController
+{
+
+    [HttpGet]
+    public IEnumerable<IDeferredRequestsHandlerInfo> Index() =>
+        manager.GetHandlers();
+
+    [HttpGet("{key:string}/[action]")]
+    public IEnumerable<ExtensionProperty> Properties(string key) =>
+        manager.GetPropertiesHandler(key);
+
+    [HttpPost]
+    public async Task<BackgroundTaskModel> AppendRequest(string key, IEnumerable<ExtensionProperty> properties, CancellationToken ct = default)
+    {
+        var task = await manager.AppendRequestAsync(key, properties, ct);
+        return Mapper.Map<BackgroundTaskModel>(task);
+    }
+    
+    
+}
