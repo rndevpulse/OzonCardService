6a1cdbfbd4153849b3bee557d2d3e64269f92629
kolur_20 <kolur_20>
2025-01-28 00:25:49 +0300

    completed key auth

--- src/net/OzonCard.Customer.Api/Authorization/ApiKeyHandler.cs
+++ src/net/OzonCard.Customer.Api/Authorization/ApiKeyHandler.cs
@@ -7,19 +7,21 @@ namespace OzonCard.Customer.Api.Authorization;
 public class ApiKeyHandler : AuthorizationHandler<ApiKeyRequirement>
 {
     private readonly IHttpContextAccessor _httpContextAccessor;
-    private static string? _key = null;
-    private static readonly string _keyHeader = "X-Api-Key";
+    private readonly string _key;
+    public readonly string Agent = "BotRestEase";
+    
     
     public ApiKeyHandler(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
     {
         _httpContextAccessor = httpContextAccessor;
-        _key ??= configuration.GetValue<string>("apiKey");
+        _key = configuration.GetValue<string>("apiKey");
     }
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context, ApiKeyRequirement requirement)
     {
-        var apiKey = _httpContextAccessor?.HttpContext?.Request.Headers[_keyHeader].ToString();
-        if (string.IsNullOrWhiteSpace(apiKey) || !_key.Equals(apiKey))
+        var apiKey = _httpContextAccessor.HttpContext?.Request.Headers["X-Api-Key"].ToString();
+        var agent = _httpContextAccessor.HttpContext?.Request.Headers.UserAgent.ToString();
+        if (!Agent.Equals(agent) || string.IsNullOrWhiteSpace(apiKey) || !_key.Equals(apiKey))
         {
             context.Fail();
             return Task.CompletedTask;
